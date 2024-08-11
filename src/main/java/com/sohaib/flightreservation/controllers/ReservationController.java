8d14a44988f51ece093a3b03f3a8b3e77e2ea65e
SohaibAli976 <59691179+SohaibAli976@users.noreply.github.com>
2024-08-11 15:22:06 +0500

    Added the Validations

--- src/main/java/com/sohaib/flightreservation/controllers/ReservationController.java
+++ src/main/java/com/sohaib/flightreservation/controllers/ReservationController.java
@@ -5,15 +5,18 @@ import com.sohaib.flightreservation.entities.Flight;
 import com.sohaib.flightreservation.entities.Reservation;
 import com.sohaib.flightreservation.repository.FlightRepository;
 import com.sohaib.flightreservation.services.ReservationService;
+import jakarta.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -37,7 +40,12 @@ public class ReservationController {
     @PostMapping("/completeReservation")
     public String completeReservation(ReservationRequest reservationRequest, ModelMap modelMap) {
         LOGGER.info("completeReservation "+reservationRequest);
-       Reservation reservation= reservationService.addFlight(reservationRequest);
+        Reservation reservation;
+        try {
+            reservation = reservationService.addFlight(reservationRequest);
+        }catch(ConstraintViolationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,e.getMessage(),e);
+        }
        modelMap.addAttribute("msg","Reservation Created Successfully and Reservation id is "+reservation.getId());
        return "reservationConfirmation";
     }
--- src/main/java/com/sohaib/flightreservation/entities/Passenger.java
+++ src/main/java/com/sohaib/flightreservation/entities/Passenger.java
@@ -1,10 +1,13 @@
 package com.sohaib.flightreservation.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -28,8 +31,14 @@ public class Passenger extends AbstractEntity{
                 '}';
     }
 
+    @NotBlank(message = "User Name is Mandatory")
+    @Length(min = 10,max = 30, message = "User Name should be 30 characters")
     private String firstName;
+    @NotBlank(message = "User Name is Mandatory")
+    @Length(min = 10,max = 30, message = "User Name should be 30 characters")
     private String lastName;
+    @NotBlank(message = "User Name is Mandatory")
+    @Length(min = 10,max = 30, message = "User Name should be 30 characters")
     private String middleName;
     private String email;
     private String phone;
